0b0f0cfb5dc558c6efc86a90324f7ac26fe976cb
Rob-Dev007 <robdev0695@gmail.com>
2024-01-06 15:40:00 -0500

    feat: implementa middleware de autenticación

---
+++ App Web Url Shortener/Backend/middlewares/authMiddleware.js
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken';
+import User from '../server/models/User.js';
+
+const CheckAuth = async(req, res, next) =>{
+    let token;
+    if(req.headers.authorization &&
+        req.headers.authorization.startsWith('Bearer')
+    ){
+        try{
+            token = req.headers.authorization.split(' ')[1];
+            console.log(token);
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+            req.user = await User.findById(decoded.id).select(" -password -token -confirmado");
+            return next();
+        }catch(error){
+            const e = new Error('Token no identificado');
+            return res.status(403).json({ msg: e.message });
+        }
+
+    }
+
+    if(!token){
+        const error = new Error('Token no válido o corrupto');
+        return res.status(400).json({ msg: error.message });
+    }
+
+}
+export default CheckAuth;
