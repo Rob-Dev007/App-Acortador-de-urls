71b7e1a1ceb8fe4df74cc13b2cbd04f749aba28b
Rob-Dev007 <robdev0695@gmail.com>
2024-12-24 00:21:29 -0500

    add: ruta publica para acortar link sin autenticación

--- Frontend/src/context/UrlProvider.jsx
+++ Frontend/src/context/UrlProvider.jsx
@@ -41,17 +41,67 @@ export const UrlProvider = ({ children })=>{
             }
             const { data } = await clienteAxios.post('/urls', url, config );
             const { __v, updateAt, ...urlAlmacenado  } = data;
-            setUrlRecortada([urlAlmacenado, ...urlRecortada]);
+            setUrlRecortada((prevUrls) => [urlAlmacenado, ...prevUrls]);
+
+            setAlerta({
+                msg: "Url recortada con exito",
+                error: false
+            })
         } catch (error) {
-            console.log(error.response.data.msg)
+            console.log(error);
         }
     };
+
+    const editarUrl = async(id, urlActualizada)=>{
+        try{
+            const token = localStorage.getItem('token');
+            const config = {
+                headers : {
+                    "Content-Type" : "application/json",
+                    Authorization : `Bearer ${token}`
+                }
+            }
+
+            const { data } = await clienteAxios.put(`/urls/${id}`, urlActualizada, config);
+
+            setUrlRecortada((prevUrls) =>
+                prevUrls.map((url) => (url._id === id ? { ...url, ...data } : url))
+            );
+
+            setAlerta({
+                msg: 'Url actualizada correctamente',
+                error: false
+            })
+
+        }catch(error){
+            console.log(error)
+        }
+    }
+
+    const eliminarUrl = async(id)=>{
+        try{
+            const token = localStorage.getItem('token');
+            const config = {
+                headers : {
+                    Authorization : `Bearer ${token}`
+                }
+            }
+
+            await clienteAxios.delete(`/urls/${id}`, config);
+
+            setUrlRecortada((prevUrls) => prevUrls.filter((url) => url._id !== id))
+        }catch(error){
+            console.log(error);
+        }
+    }
 
     return(
         <UrlContext.Provider
             value={{
                 urlRecortada,
-                guardarUrl
+                guardarUrl,
+                editarUrl,
+                eliminarUrl
             }}
         >
             { children }
